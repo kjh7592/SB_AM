8c548598a93ea75905b4074a2d2e23a378929e72
kjh7592 <sajh0405@gmail.com>
2022-12-02 19:27:29 +0900

    writeArticle 매서드를 활용해서 게시물 테스트데이터를 10개 생성

---
+++ src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
@@ -0,0 +1,60 @@
+package com.kjh.exam.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kjh.exam.demo.vo.Article;
+
+@Controller
+public class UsrArticleController {
+	
+	private int lastArticleId;
+	private List<Article> articles;
+	
+	public UsrArticleController() {
+		this.lastArticleId = 0;
+		this.articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+	
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
+	private Article writeArticle(String title, String body) {
+		int id = lastArticleId + 1;
+		Article article = new Article(id, title, body);
+		
+		articles.add(article);
+		lastArticleId = id;
+		
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		
+		return writeArticle(title, body);
+	}
+	
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		
+		return articles;
+	}
+	
+	
+}
