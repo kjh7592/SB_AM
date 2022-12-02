10f4c8a615746f48f2997bd2d69a3d320bdd9fba
kjh7592 <sajh0405@gmail.com>
2022-12-02 20:19:39 +0900

    게시물 수정 기능 구현

--- src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
@@ -41,6 +41,30 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
+	public Article getArticle(int id) {
+		
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+		
+	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
@@ -49,6 +73,7 @@ public class UsrArticleController {
 		return writeArticle(title, body);
 	}
 	
+	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
@@ -56,5 +81,36 @@ public class UsrArticleController {
 		return articles;
 	}
 	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id + "번 글은 존재하지 않습니다";
+		}
+		
+		deleteArticle(id);
+		
+		return id + "번 글이 삭제되었습니다";
+	}
+	
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+		
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id + "번 글은 존재하지 않습니다";
+		}
+		
+		modifyArticle(id, title, body);
+		
+		return article;
+	}
+
 	
 }
