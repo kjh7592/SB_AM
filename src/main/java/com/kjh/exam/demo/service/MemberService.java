b7cd85f13b710da98fcd0ac550ea0d4e9d46c1a1
kjh7592 <sajh0405@gmail.com>
2022-12-06 20:48:53 +0900

    doJoin기능 구현, 가입 후 회원정보 나오도록/로그인아이디 중복체크, 회원가입시 필요한 데이터 유효성 체크

--- src/main/java/com/kjh/exam/demo/service/MemberService.java
+++ src/main/java/com/kjh/exam/demo/service/MemberService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kjh.exam.demo.repository.MemberRepository;
+import com.kjh.exam.demo.vo.Member;
 
 @Service
 public class MemberService {
@@ -16,10 +17,25 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public void doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public int doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 		
-//		memberRepository.doJoin(loginId, loginPw, name, nickname, cellphoneNum, eamil);
+		Member existsMember = getMemberByLoginId(loginId);
+		
+		if(existsMember != null) {
+			return -1;
+		}
+		
+		memberRepository.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+		return memberRepository.getLastInssertId();
+	}
+
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
+	}
+	
+	public Member getMemberByLoginId(String LoginId) {
+		return memberRepository.getMemberByLoginId(LoginId);
 	}
 	
 }
