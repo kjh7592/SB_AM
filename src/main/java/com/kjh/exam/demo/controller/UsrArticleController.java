7fa3a6828cf098f1678f27948c2bc25436333c3a
kjh7592 <sajh0405@gmail.com>
2022-12-08 19:35:02 +0900

    getArticles, getArticle에 제네릭 적용. doModify, doDelete에 ResultData 적용(제네릭 포함)

--- src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
@@ -45,7 +45,7 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public ResultData getArticles() {
+	public ResultData<List<Article>> getArticles() {
 
 		List<Article> articles = articleService.getArticles();
 		return ResultData.from("S-1", "게시물 리스트", articles);
@@ -53,37 +53,37 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData<Integer> doDelete(int id) {
 
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 글은 존재하지 않습니다";
-		}
+			return ResultData.from("F-1", Utility.f("%d번 게시물은 존재하지 않습니다", id));
+			}
 
 		articleService.deleteArticle(id);
 
-		return id + "번 글이 삭제되었습니다";
+		return ResultData.from("S-1", Utility.f("%d번 게시물을 삭제했습니다", id), id);		
 	}
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public Object doModify(int id, String title, String body) {
+	public ResultData<Integer> doModify(int id, String title, String body) {
 
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 글은 존재하지 않습니다";
+			return ResultData.from("F-1", Utility.f("%d번 게시물은 존재하지 않습니다", id));
 		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return article;
+		return ResultData.from("S-1", Utility.f("%d번 게시물을 수정습니다", id), id);
 	}
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public ResultData getArticle(int id) {
+	public ResultData<Article> getArticle(int id) {
 
 		Article article = articleService.getArticle(id);
 
