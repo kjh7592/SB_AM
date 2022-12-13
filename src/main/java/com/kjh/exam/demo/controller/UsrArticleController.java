5f15b4b3a3b0e10d30899f6bbc05c8317d30bbeb
kjh7592 <sajh0405@gmail.com>
2022-12-13 19:48:48 +0900

    List, Detail에 작성자 닉네임 표시. Detail에 수정, 삭제 버튼 추가

--- src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kjh/exam/demo/controller/UsrArticleController.java
@@ -115,7 +115,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/detail")
 	public String detail(Model model, int id) {
 
-		Article article = articleService.getArticle(id);
+		Article article = articleService.getForPrintArticle(id);
 
 		model.addAttribute("article", article);
 		
