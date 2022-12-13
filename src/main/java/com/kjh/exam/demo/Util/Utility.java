8f1c9ec4e6a2134a7b038bac9bc8192271c01188
kjh7592 <sajh0405@gmail.com>
2022-12-13 20:55:24 +0900

    doDelete 후 history.back or location.replace 처리

--- src/main/java/com/kjh/exam/demo/Util/Utility.java
+++ src/main/java/com/kjh/exam/demo/Util/Utility.java
@@ -1,5 +1,7 @@
 package com.kjh.exam.demo.Util;
 
+import com.kjh.exam.demo.vo.ResultData;
+
 public class Utility {
 
 	public static boolean empty(Object obj) {
@@ -19,6 +21,43 @@ public class Utility {
 
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
+	}
+
+	public static String jsHistoryBack(String msg) {
+		
+		if(msg == null) {
+			msg = "";
+		}
+		
+		return Utility.f("""
+						<script>
+							const msg = '%s'.trim();
+							if(msg.length > 0){
+								alert(msg);
+							}
+							history.back();
+						</script>
+						""", msg);
+	}
+
+	public static String jsReplace(String msg, String uri) {
+		if(msg == null) {
+			msg = "";
+		}
+		
+		if(uri == null) {
+			uri = "";
+		}
+		
+		return Utility.f("""
+						<script>
+							const msg = '%s'.trim();
+							if(msg.length > 0){
+								alert(msg);
+							}
+							location.replace('%s')
+						</script>
+						""", msg, uri);
 	}
 
 }
